81ac2b5b854ef14a805521d9ad402d54f79a9d33
yossefazoulay <azoulayos@protonmail.com>
2020-10-14 14:45:39 +0300

    add getUserId func

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -2,8 +2,9 @@ package oauth
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	errors "github.com/micro-gis/utils/rest_errors"
+	errors2 "github.com/micro-gis/utils/rest_errors"
 	"github.com/yossefaz/go-http-client/gohttp"
 	"net/http"
 	"strconv"
@@ -39,7 +40,7 @@ func IsPublic(request *http.Request) bool {
 	return request.Header.Get(headerPublic) == "true"
 }
 
-func AuthenticateRequest(request *http.Request) errors.RestErr{
+func AuthenticateRequest(request *http.Request) errors2.RestErr{
 	if request == nil {
 		return nil
 	}
@@ -52,7 +53,7 @@ func AuthenticateRequest(request *http.Request) errors.RestErr{
 	at, err := getAccessToken(accessTokenId)
 	if err != nil {
 		if err.Status() == http.StatusNotFound{
-			return errors.NewUnauthorizedError("unknown token provided")
+			return errors2.NewUnauthorizedError("unknown token provided")
 		}
 		return err
 	}
@@ -71,6 +72,24 @@ func GetCallerId(request *http.Request) int64 {
 		return 0
 	}
 	return callerId
+}
+
+func GetUserId(request *http.Request) (int64, errors2.RestErr) {
+	accessTokenId := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenId == ""{
+		return 0, errors2.NewUnauthorizedError("access token is missing")
+	}
+	at, err := getAccessToken(accessTokenId)
+	if err != nil {
+		if err.Status() == http.StatusNotFound{
+			return 0, errors2.NewUnauthorizedError("unknown token provided")
+		}
+		return 0, err
+	}
+	if request == nil {
+		return 0, errors2.NewInternalServerError("bad request", errors.New("trying to get user id : request params is not valid"))
+	}
+	return at.UserId, nil
 }
 
 func GetClientId(request *http.Request) int64{
@@ -93,26 +112,26 @@ func cleanRequest(request *http.Request) {
 	request.Header.Del(headerXCallerId)
 }
 
-func getAccessToken(accessTokenId string)(*accessToken, errors.RestErr){
+func getAccessToken(accessTokenId string)(*accessToken, errors2.RestErr){
 	response, err := oauthRestClient.Get(fmt.Sprintf("http://127.0.0.1:8087/oauth/access_token/%s",accessTokenId))
 	if err != nil {
-		return nil, errors.NewBadRequestError("invalid access token provided")
+		return nil, errors2.NewBadRequestError("invalid access token provided")
 	}
 	if response == nil || response.StatusCode < 100 {
-		return nil, errors.NewInternalServerError("invalid restClient response when trying to get access token", err)
+		return nil, errors2.NewInternalServerError("invalid restClient response when trying to get access token", err)
 	}
 
 	if response.StatusCode > 299 {
-		restErr, err := errors.NewRestErrorFromBytes(response.Bytes())
+		restErr, err := errors2.NewRestErrorFromBytes(response.Bytes())
 		if err != nil {
-			return nil, errors.NewInternalServerError("invalid error interface when trying to login user", err)
+			return nil, errors2.NewInternalServerError("invalid error interface when trying to login user", err)
 		}
 		return nil, restErr
 	}
 
 	var at accessToken
 	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
-		return nil, errors.NewInternalServerError("error when trying to unmarshall access token", err)
+		return nil, errors2.NewInternalServerError("error when trying to unmarshall access token", err)
 	}
 	return &at, nil
 }
